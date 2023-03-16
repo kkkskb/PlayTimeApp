8d7461f190d54bb9ad7e16390502f8b09f593907
koki <k.kusakabe1030@gmail.com>
2023-03-16 16:49:36 +0900

    Initial Commit

---
+++ PlayTime/PlayTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayTimeApp.swift
+//  PlayTime
+//
+//  Created by Kusakabe Koki on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct PlayTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
